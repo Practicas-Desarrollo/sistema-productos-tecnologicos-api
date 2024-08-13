e99581376cc9486b7e4f21fc2e62cd4943335152
José Luis Terán <joseluisteran.study@gmail.com>
2024-08-13 18:42:23 -0400

    build: update package path

--- src/main/java/proyecto/desarollo/api/SistemaProductosTecnologicosApiApplication.java
+++ src/main/java/proyecto/desarollo/api/SistemaProductosTecnologicosApiApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package proyecto.desarollo.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
